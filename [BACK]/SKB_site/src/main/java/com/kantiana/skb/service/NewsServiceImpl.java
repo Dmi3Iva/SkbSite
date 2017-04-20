689a79401f3c38e65fcb3e7d608d17d66f93252c
AntonD <d.flier@yandex.ru>
2017-04-20 22:09:21 +0200

    Подключена xml, настраивающий работу с бд.
    Добавлена передача всех новостей в index.jsp.

--- [BACK]/SKB_site/src/main/java/com/kantiana/skb/service/NewsServiceImpl.java
+++ [BACK]/SKB_site/src/main/java/com/kantiana/skb/service/NewsServiceImpl.java
@@ -3,9 +3,11 @@ package com.kantiana.skb.service;
 import com.kantiana.skb.model.News;
 import com.kantiana.skb.repository.NewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class NewsServiceImpl implements NewsService {
     @Autowired
     private NewsRepository newsRepository;
