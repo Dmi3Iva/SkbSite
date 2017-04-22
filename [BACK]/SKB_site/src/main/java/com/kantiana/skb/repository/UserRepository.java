e6f2a15f7c1a5d95d03bb47d68ec59e68d6a5881
AntonD <d.flier@yandex.ru>
2017-04-22 13:59:07 +0200

    Всё исправлено. Честно.

--- [BACK]/SKB_site/src/main/java/com/kantiana/skb/repository/UserRepository.java
+++ [BACK]/SKB_site/src/main/java/com/kantiana/skb/repository/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kantiana.skb.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    User findByName(String name);
 }
--- [BACK]/Scripts/db.sql
+++ [BACK]/Scripts/db.sql
@@ -12,8 +12,8 @@ CREATE TABLE users (
 DROP TABLE IF EXISTS news CASCADE;
 CREATE TABLE news (
 	id SERIAL NOT NULL PRIMARY KEY,
-	author_id INT NOT NULL REFERENCES users(id),
+	authorId INT NOT NULL REFERENCES users(id),
 	name VARCHAR(255) DEFAULT NULL,
 	article TEXT DEFAULT NULL,
-	date_of_creation DATE NOT NULL
+	dateOfCreation DATE NOT NULL
 );
