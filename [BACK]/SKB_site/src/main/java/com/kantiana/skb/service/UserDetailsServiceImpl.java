448446e6c5c7059f1a30807f9451c3854ff8f26c
AntonD <d.flier@yandex.ru>
2017-04-29 10:33:45 +0200

    Приписан @Service в UserDetailsServiceImpl

--- [BACK]/SKB_site/src/main/java/com/kantiana/skb/service/UserDetailsServiceImpl.java
+++ [BACK]/SKB_site/src/main/java/com/kantiana/skb/service/UserDetailsServiceImpl.java
@@ -7,11 +7,13 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
