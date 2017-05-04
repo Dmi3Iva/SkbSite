aff0a2958306af11cf2d1a9d43ecaf0e8d56d739
AntonD <d.flier@yandex.ru>
2017-05-04 13:59:12 +0200

    Уменьшен необходимый размер логина, пароля для удобства.

--- [BACK]/SKB_site/src/main/java/com/kantiana/skb/validator/UserValidator.java
+++ [BACK]/SKB_site/src/main/java/com/kantiana/skb/validator/UserValidator.java
@@ -24,7 +24,7 @@ public class UserValidator implements Validator {
 
         // Проверка имени пользователя
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "NotEmpty");
-        if (user.getUsername().length() < 6 || user.getUsername().length() > 32) {
+        if (user.getUsername().length() < 2 || user.getUsername().length() > 32) {
             errors.rejectValue("username", "Size.userForm.username");
         }
         if (userService.findByUsername(user.getUsername()) != null) {
@@ -33,7 +33,7 @@ public class UserValidator implements Validator {
 
         // Проверка пароля
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
-        if (user.getPassword().length() < 8 || user.getPassword().length() > 32) {
+        if (user.getPassword().length() < 2 || user.getPassword().length() > 32) {
             errors.rejectValue("password", "Size.userForm.password");
         }
         if (!user.getPasswordConfirm().equals(user.getPassword())) {
