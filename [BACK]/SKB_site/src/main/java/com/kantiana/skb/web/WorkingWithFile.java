97f889d7c49fc1ea70afe3544ccb3a7891c7aac9
DmitriyIvanov <Diklacway@gmail.com>
2017-06-26 14:12:31 +0200

    Удалось вывести картинку
    Местохранение папка Images  в папке репозитория

--- [BACK]/SKB_site/src/main/java/com/kantiana/skb/web/WorkingWithFile.java
+++ [BACK]/SKB_site/src/main/java/com/kantiana/skb/web/WorkingWithFile.java
@@ -1,22 +1,49 @@
 package com.kantiana.skb.web;
 
 import org.apache.commons.fileupload.FileUpload;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import sun.nio.ch.IOUtil;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.awt.*;
+import java.io.*;
 import java.util.Random;
 
-/**
- * Created by FTL on 26.05.2017.
- */
+@Controller
 public class WorkingWithFile {
 
     private static final Logger logger = LoggerFactory.getLogger(FileUpload.class);
 
+
+    @ResponseBody
+    @RequestMapping(value = "/images/{name}.jpg", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] imageJPGOnThePage() throws Exception{
+        InputStream in = null;
+        File f = new File("");
+        in = new FileInputStream(f);
+
+        return IOUtils.toByteArray(in);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/images/{name}.png", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] imagePNGOnThePage(@PathVariable String name) throws Exception{
+        InputStream in = null;
+        String rootPath = System.getProperty("user.dir");
+        File f = new File(rootPath+"/../images/"+name+".png");
+        in = new FileInputStream(f);
+
+
+        return IOUtils.toByteArray(in);
+    }
 
     static public void  UploadFile(){
     }
@@ -33,7 +60,7 @@ public class WorkingWithFile {
 
                 String rootPath = System.getProperty("user.dir");
                 //Директория
-                File dir = new File(rootPath+"\\..\\webapps\\ROOT\\resources\\images\\");
+                File dir = new File(rootPath+"/../images/");
 
                 if (!dir.exists()) {
                     dir.mkdirs();
@@ -41,13 +68,13 @@ public class WorkingWithFile {
 
                 File uploadedFile = new File(dir.getAbsolutePath() + File.separator + name);
                 Random a= new Random();
-                res = "/resources/images/"+name;
+                res = "/images/"+name;
                 String random = new String();
                 while(uploadedFile.exists())
                 {
                     random = String.valueOf(a.nextInt());
                     uploadedFile = new File(dir.getAbsolutePath() + File.separator + random + name);
-                    res = "/resources/images/"+random +name;
+                    res = "/images/"+random +name;
                 }
 
                 BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(uploadedFile));
