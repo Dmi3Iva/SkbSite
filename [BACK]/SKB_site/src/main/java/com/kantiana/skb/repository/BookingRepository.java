5cc916f0b108b758a374b6296f4276ad61628a38
AntonD <d.flier@yandex.ru>
2017-06-26 13:44:21 +0200

    Запрос на List<Booking> для заданного дня и типа оборудования

--- [BACK]/SKB_site/src/main/java/com/kantiana/skb/repository/BookingRepository.java
+++ [BACK]/SKB_site/src/main/java/com/kantiana/skb/repository/BookingRepository.java
@@ -2,9 +2,14 @@ package com.kantiana.skb.repository;
 
 import com.kantiana.skb.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.sql.Date;
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-        public Booking findById(Long id);
+    Booking findById(Long id);
+
+    @Query("SELECT b FROM Booking b WHERE b.day = ?1 AND b.equipment.equipmentType.id = ?2")
+    List<Booking> findByDayAndEquipmentType(Date day, Long equipmentTypeId);
 }
