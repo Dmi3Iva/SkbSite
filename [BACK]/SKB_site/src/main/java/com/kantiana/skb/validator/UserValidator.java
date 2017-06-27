9cbe2ea2beb96314744a1c0d9892fe663ffd2dba
AntonD <d.flier@yandex.ru>
2017-06-27 17:13:17 +0200

    Делаю вывод ошибок при регистрации

--- [BACK]/SKB_site/src/main/java/com/kantiana/skb/validator/UserValidator.java
+++ [BACK]/SKB_site/src/main/java/com/kantiana/skb/validator/UserValidator.java
@@ -2,6 +2,7 @@ package com.kantiana.skb.validator;
 
 import com.kantiana.skb.model.User;
 import com.kantiana.skb.service.UserService;
+import org.hibernate.validator.internal.constraintvalidators.hv.URLValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -24,18 +25,34 @@ public class UserValidator implements Validator {
         // Проверка имени пользователя
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "NotEmpty");
         if (user.getUsername().length() < 2 || user.getUsername().length() > 32) {
-            errors.rejectValue("username", "Size.userForm.username");
+            errors.rejectValue("username", "Size.user.username");
         }
         if (userService.findByUsername(user.getUsername()) != null) {
-            errors.rejectValue("username", "Duplicate.userForm.username");
+            errors.rejectValue("username", "Duplicate.user.username");
         }
         // Проверка пароля
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
         if (user.getPassword().length() < 2 || user.getPassword().length() > 32) {
-            errors.rejectValue("password", "Size.userForm.password");
+            errors.rejectValue("password", "Size.user.password");
         }
         if (!user.getPasswordConfirm().equals(user.getPassword())) {
-            errors.rejectValue("passwordConfirm", "Diff.userForm.passwordConfirm");
+            errors.rejectValue("passwordConfirm", "Diff.user.passwordConfirm");
         }
+        // Проверка ФИО
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "NotEmpty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "NotEmpty");
+        // Проверка организации
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "organization", "NotEmpty");
+        // Проверка github
+        URLValidator urlValidator = new URLValidator();
+        if (!urlValidator.isValid(user.getEmail(), null)) {
+            errors.rejectValue("email", "NotValid.link");
+        }
+        // Проверка даты
+        //TODO: Проверка формата даты?
+        //TODO: Дата не может быть больше сегодняшней
+        // Проверка email
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "NotEmpty");
+        //TODO: Проверка формата почты
     }
 }
